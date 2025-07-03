525d26e78482e9803120714301178e09bb52f401
Paola Andrade <paolita31@gmail.com>
2025-07-03 14:03:45 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,10 @@
+URL_SERVICE = "https://cnt-5dae734a-1cea-4942-9671-a7bcdd506dac.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
